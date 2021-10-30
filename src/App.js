1684d664f1cb5eaf94221d2bc003947ce1706b06
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-10-30 13:25:02 +0700

    add animation (firstTime)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import { ChakraProvider } from "@chakra-ui/react"
 import React from 'react'
+import { Box, Center, Heading } from '@chakra-ui/layout';
+import { motion } from 'framer-motion';
+
+const MotionBox = motion(Box)
+const MotionHeading = motion(Heading)
+
+const LoadingVariants = {
+  hidden: {
+    x: 0,
+  },
+  visible: {
+    x: "-100%",
+    transition:{
+      type: "linear",
+      delay: 3,
+      duration: 0.5,
+      when: "afterChildren"
+    }
+  }
+}
 
 
 
 function App() {
   return (
     <ChakraProvider>
+      <MotionBox 
+      variants={LoadingVariants} initial="hidden" animate="visible" 
+      className="loading"
+      >
+        <Center h="100%">
+          <MotionHeading animate={{opacity: [0,1,1,1,0], transition: {duration: 3}}} color="white">
+            Welcome
+          </MotionHeading>
+        </Center>
+
+      </MotionBox>
       <div className="App">
         <Navbar />
         <div className="App-Content">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,71 @@ import { Image } from "@chakra-ui/image";
 import Me from "../assets/Me2.png";
 import { Button } from "@chakra-ui/button";
 import '../App.css';
+import { motion } from "framer-motion";
 
+const MotionBox = motion(Box)
+const MotionHeading = motion(Heading)
+const MotionText = motion(Text)
+const MotionFlex = motion(Flex)
+
+const BaseVariants = {
+    hidden: {
+        y: -500,
+        opacity: 0
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: 3.5,
+            type: "linear",
+            duration: 0.5,
+            delayChildren: 3
+        }
+    }
+}
+
+const NextVariants = {
+    hidden: {
+        opacity: 0,
+        y: -50
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition:{
+            type: "spring",
+            damping: 3,
+            // delay: 0.8
+        }
+    }
+}
+
+const NextVariants2 = {
+    hidden: {
+        opacity: 0,
+        y: 50
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition:{
+            type: "spring",
+            damping: 3,
+            // delay: 0.8
+        }
+    }
+}
 
 const Home = () => {
     return (
-        <Box backgroundImage={bgBox} px={50} pb={60} pt={70} h={["100%", "100%", "500px", "500px"]}>
+        <MotionBox  backgroundImage={bgBox} px={50} pb={60} pt={70} h={["100%", "100%", "500px", "500px"]}  variants={BaseVariants} initial="hidden" animate="visible">
             <SimpleGrid columns={[1, null, 2]}>
-                <Box color="white" w="100%">
-                    <Heading bgGradient="linear(to-r, #FF0075, #172774, #FF0075)" bgClip="text" fontSize="70px" style={{ fontWeight: '500' }} lineHeight="70px" >Hi I am <br /> Najim <br />Rizky</Heading>
-                    <Text mt={[10]} lineHeight="30px" fontFamily="Raleway" fontWeight="thin" fontSize="14">
+                <MotionBox variants={NextVariants} color="white" w="100%">
+                    <MotionHeading  bgGradient="linear(to-r, #FF0075, #172774, #FF0075)" bgClip="text" fontSize="70px" style={{ fontWeight: '500' }} lineHeight="70px" >Hi I am <br /> Najim <br />Rizky</MotionHeading>
+                    <MotionText  mt={[10]} lineHeight="30px" fontFamily="Raleway" fontWeight="thin" fontSize="14">
                         A <i><b>Junior IT Programmer</b></i> who is currently<br /> studying at Multimedia Nusantara University
-                    </Text>
+                    </MotionText>
                     <Button
                         className="downloadCVBtn"
                         bgGradient="linear(to-r, #ff00d4, #7407f1, #196ad4)"
@@ -24,12 +78,12 @@ const Home = () => {
                         _hover={{
                             bgPosition: "right"
                         }} my={[10]} color="black">Download CV</Button>
-                </Box>
-                <Flex justify={['left',"left","end","center"]} color="white" w="100%" >
+                </MotionBox>
+                <MotionFlex variants={NextVariants2} justify={['left',"left","end","center"]} color="white" w="100%" >
                     <Image src={Me} mt={[0,0,10,10]} objectFit="cover" htmlWidth={["350px","350px","100%","100%"]} />
-                </Flex>
+                </MotionFlex>
             </SimpleGrid>
-        </Box>
+        </MotionBox>
     );
 }
 
