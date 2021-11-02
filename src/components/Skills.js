c37a5947084a7550e359fa63ab3c87ab76cbe455
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-11-02 17:31:04 +0700

    add animation skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,14 @@
 import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel } from "@chakra-ui/accordion";
 import { Box,Flex, Heading, HStack, Spacer, Text } from "@chakra-ui/layout";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import bgBox from "../assets/bgBoxHome.svg";
 
 const MotionBox = motion(Box);
+const MotionAccordionPanel = motion(AccordionPanel);
+const MotionFlex = motion(Flex);
+const MotionHStack = motion(HStack);
+
+
 const programming = ([
     { lang: "HTML", rank: "Intermediate", levelSkil: "80%", levelIdx: "20%", id: 1 },
     { lang: "CSS", rank: "Intermediate", levelSkil: "70%", levelIdx: "30%", id: 2 },
@@ -20,9 +25,38 @@ const language = ([
     { lang: "English", rank: "Intermediate", levelSkil: "60%", levelIdx: "40%", id: 2 },
 ]);
 
+const BaseVariants = {
+    hidden: {
+        y: -100,
+    },
+    visible: {
+        y: 0,
+        transition: {
+            when: "beforeChildren",
+            staggerChildren: 0.3
+        }
+    }
+}
+
+const NextVariants = {
+    hidden: {
+        x: -400,
+    },
+    visible: {
+        x: 0,
+        transition: {
+            type: "linear",
+            duration: 0.3
+        }
+    },
+    exit: {
+        opacity: 0
+    }
+}
+
 const Skills = () => {
     return (
-        <Box id="skills" color="white" backgroundImage={bgBox} px={[1,1,20,20]} pb={[10, 10, 30, 30]} pt={70} fontFamily="Raleway">
+        <MotionBox  id="skills" color="white" backgroundImage={bgBox} px={[1,1,20,20]} pb={[10, 10, 30, 30]} pt={70} fontFamily="Raleway">
             <Heading mb={10} px={[5,5,0,0]} fontWeight={400} fontSize={50} >
                 Skills
             </Heading>
@@ -34,10 +68,12 @@ const Skills = () => {
                         </Box>
                         <AccordionIcon />
                     </AccordionButton>
-                    <AccordionPanel pb={4}>
-                    {programming.map((item) => (
-                        <Flex alignItems="center" overflow="hidden" my={7} w="100%" key={item.id}>
-                            <HStack w="90%" color="white" alignItems="center" spacing={[2,2,8,8]} overflow="hidden">
+                    <MotionAccordionPanel pb={4} >
+                    <AnimatePresence >
+                    <motion.div variants={BaseVariants} initial="hidden" animate="visible" >
+                    {programming.length && programming.map((item) => (
+                        <MotionFlex  alignItems="center"  overflow="hidden" my={7} w="100%" key={item.id}>
+                            <MotionHStack variants={NextVariants} w="90%" color="white" alignItems="center" spacing={[2,2,8,8]} overflow="hidden">
                                 <MotionBox bg="white" w={item.levelSkil} h="40px" p="2" color="black" >
                                     <Flex>
                                         <Text fontWeight="bold">{item.lang}</Text>
@@ -46,12 +82,14 @@ const Skills = () => {
                                     </Flex>
                                     {/* {item.lang} */}
                                 </MotionBox>
-                                <MotionBox w={item.levelIdx} h="3px" bg="gray"></MotionBox>
-                            </HStack>
+                                <MotionBox  w={item.levelIdx} h="3px" bg="gray"></MotionBox>
+                            </MotionHStack>
                             <Text w="100px" ml={[2,2,8,8]} >{item.rank}</Text>
-                        </Flex>
+                        </MotionFlex>
                     ))}
-                    </AccordionPanel>
+                    </motion.div>
+                    </AnimatePresence>
+                    </MotionAccordionPanel>
                 </AccordionItem>
                 <AccordionItem >
                     <AccordionButton _hover={{bgColor: "rgba(255,255,255,0.1)"}} borderRadius="20px" transition="0.2s" border="none">
@@ -80,7 +118,7 @@ const Skills = () => {
                     </AccordionPanel>
                 </AccordionItem>
             </Accordion>
-        </Box>
+        </MotionBox>
     );
 }
 
