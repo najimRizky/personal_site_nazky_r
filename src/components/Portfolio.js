a2f6c534c387bb72918b42828360f83cce4e7f8d
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-11-05 22:17:36 +0700

    fix card portfolio prep

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import { Box, Center, Heading, HStack, SimpleGrid, Text } from "@chakra-ui/layout";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
+import bgPortfolio from "../assets/large-triangles.svg"
 import BgShape from "../assets/Wave.svg";
 import { Button } from "@chakra-ui/button";
 import { Image } from "@chakra-ui/image";
@@ -21,12 +22,12 @@ const Portfolio = () => {
 
 
     return (
-        <Box id={id} bgColor="white" bgImage={BgShape} bgSize="100%" px={[5, 5, 20, 20]} pt={70} fontFamily="Raleway">
+        <Box id={id} bgColor="white" bgImage={bgPortfolio} bgSize="100%" px={[5, 5, 20, 20]} pt={70} fontFamily="Raleway">
             <Heading ref={ref} mb={10} fontWeight={400} fontSize={50} >
                 Portfolio
             </Heading>
-            <SimpleGrid columns={[1, 1, 1, 2]} justifyItems="center">
-                <MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
+            <SimpleGrid columns={[1, 1, 1, 2]}  justifyItems="center">
+                <Box w="90%" my={5} className="portfolioCard" bg="blackAlpha.100">
                     <HStack>
                         <Box p={2} w="30%" >
                             <Center>
@@ -42,120 +43,7 @@ const Portfolio = () => {
                             </Button>
                         </Box>
                     </HStack>
-                </MotionBox>
-                <MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
-                    <HStack>
-                        <Box p={2} w="30%" >
-                            <Center>
-                                <Image boxSize={20} alignSelf="center" src={imgNYLCinema21} />
-                            </Center>
-                        </Box>
-                        <Box w="70%" p={2} h="150px">
-                            <Heading size="md">NYL Cinema 21</Heading>
-                            <Text my={2}>Movie Information Site</Text>
-                            <Text my={2}>December 19</Text>
-                            <Button size="sm">
-                                Visit
-                            </Button>
-                        </Box>
-                    </HStack>
-                </MotionBox><MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
-                    <HStack>
-                        <Box p={2} w="30%" >
-                            <Center>
-                                <Image boxSize={20} alignSelf="center" src={imgNYLCinema21} />
-                            </Center>
-                        </Box>
-                        <Box w="70%" p={2} h="150px">
-                            <Heading size="md">NYL Cinema 21</Heading>
-                            <Text my={2}>Movie Information Site</Text>
-                            <Text my={2}>December 19</Text>
-                            <Button size="sm">
-                                Visit
-                            </Button>
-                        </Box>
-                    </HStack>
-                </MotionBox><MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
-                    <HStack>
-                        <Box p={2} w="30%" >
-                            <Center>
-                                <Image boxSize={20} alignSelf="center" src={imgNYLCinema21} />
-                            </Center>
-                        </Box>
-                        <Box w="70%" p={2} h="150px">
-                            <Heading size="md">NYL Cinema 21</Heading>
-                            <Text my={2}>Movie Information Site</Text>
-                            <Text my={2}>December 19</Text>
-                            <Button size="sm">
-                                Visit
-                            </Button>
-                        </Box>
-                    </HStack>
-                </MotionBox><MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
-                    <HStack>
-                        <Box p={2} w="30%" >
-                            <Center>
-                                <Image boxSize={20} alignSelf="center" src={imgNYLCinema21} />
-                            </Center>
-                        </Box>
-                        <Box w="70%" p={2} h="150px">
-                            <Heading size="md">NYL Cinema 21</Heading>
-                            <Text my={2}>Movie Information Site</Text>
-                            <Text my={2}>December 19</Text>
-                            <Button size="sm">
-                                Visit
-                            </Button>
-                        </Box>
-                    </HStack>
-                </MotionBox><MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
-                    <HStack>
-                        <Box p={2} w="30%" >
-                            <Center>
-                                <Image boxSize={20} alignSelf="center" src={imgNYLCinema21} />
-                            </Center>
-                        </Box>
-                        <Box w="70%" p={2} h="150px">
-                            <Heading size="md">NYL Cinema 21</Heading>
-                            <Text my={2}>Movie Information Site</Text>
-                            <Text my={2}>December 19</Text>
-                            <Button size="sm">
-                                Visit
-                            </Button>
-                        </Box>
-                    </HStack>
-                </MotionBox><MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
-                    <HStack>
-                        <Box p={2} w="30%" >
-                            <Center>
-                                <Image boxSize={20} alignSelf="center" src={imgNYLCinema21} />
-                            </Center>
-                        </Box>
-                        <Box w="70%" p={2} h="150px">
-                            <Heading size="md">NYL Cinema 21</Heading>
-                            <Text my={2}>Movie Information Site</Text>
-                            <Text my={2}>December 19</Text>
-                            <Button size="sm">
-                                Visit
-                            </Button>
-                        </Box>
-                    </HStack>
-                </MotionBox><MotionBox w="90%" my={5} borderRadius="15px" overflow="hidden" bg="blackAlpha.300" transition="0.3s">
-                    <HStack>
-                        <Box p={2} w="30%" >
-                            <Center>
-                                <Image boxSize={20} alignSelf="center" src={imgNYLCinema21} />
-                            </Center>
-                        </Box>
-                        <Box w="70%" p={2} h="150px">
-                            <Heading size="md">NYL Cinema 21</Heading>
-                            <Text my={2}>Movie Information Site</Text>
-                            <Text my={2}>December 19</Text>
-                            <Button size="sm">
-                                Visit
-                            </Button>
-                        </Box>
-                    </HStack>
-                </MotionBox>
+                </Box>
             </SimpleGrid>
         </Box>
     );
