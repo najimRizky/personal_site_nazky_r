e1b7acf08a4a4acdaf0892328b449006af1c6377
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-11-07 20:27:57 +0700

    add react router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import Footer from "../components/Footer";
 const Home = () => {
     return (
         <>
-            <Greetings />
+            <Greetings/>
             <Aboutme/>
             <Skills/>
             <Portfolio/>
