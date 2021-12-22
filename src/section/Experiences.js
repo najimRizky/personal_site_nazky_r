2f7cc877f692c3159add69c26018b2b37e0c85a1
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-12-22 12:41:06 +0700

    change responsive title Experiences section

--- src/section/Experiences.js
+++ src/section/Experiences.js
@@ -17,25 +17,23 @@ const Experiences = (props) => {
         }
     }, [inView]);
     return (
-        <Box id={id} ref={ref} mt="-1px" pb={10} px={[5]} pt={20} backgroundImage={props.bgBox} transition={props.transition} backgroundColor={props.theme} color={props.color}>
-            <Box px={12} >
-                <Heading color={props.color} mb={10} fontWeight={400} fontSize={50} >
-                    EXPERIENCES
-                </Heading>
-            </Box>
+        <Box id={id} ref={ref} mt="-1px" pb={10} px={[5,5,10,20]} pt={20} backgroundImage={props.bgBox} transition={props.transition} backgroundColor={props.theme} color={props.color}>
+            <Heading color={props.color} mb={10} fontWeight={400} fontSize={50} >
+                EXPERIENCES
+            </Heading>
             <VerticalTimeline lineColor={props.color}>
                 {listExperiences.map((experience) =>
                     <VerticalTimelineElement
-                    key={experience.id}
-                    className="horizontal-timeline-element--work"
-                    contentStyle={{ borderTop: `2px solid ${experience.topLine}`, borderBottom: "none", background: props.theme === "#FFFFFF" ? "#edeff2" : "#242c3d" }}
-                    contentArrowStyle={{ borderRight: `7px solid  ${props.theme === "#FFFFFF" ? "#edeff2" : "#242c3d"}` }}
-                    date={experience.date}
-                    iconStyle={{ background: experience.type === "education" ? "#287099" : "#242c3d", color: '#fff'}}
-                    icon={<FontAwesomeIcon icon={experience.icon} size="lg"/>}
+                        key={experience.id}
+                        className="horizontal-timeline-element--work"
+                        contentStyle={{ borderTop: `2px solid ${experience.topLine}`, borderBottom: "none", background: props.theme === "#FFFFFF" ? "#edeff2" : "#242c3d" }}
+                        contentArrowStyle={{ borderRight: `7px solid  ${props.theme === "#FFFFFF" ? "#edeff2" : "#242c3d"}` }}
+                        date={experience.date}
+                        iconStyle={{ background: experience.type === "education" ? "#287099" : "#242c3d", color: '#fff' }}
+                        icon={<FontAwesomeIcon icon={experience.icon} size="lg" />}
                     >
-                        <h1 className="vertical-timeline-element-title" style={{fontSize: "22px"}}>{experience.place}</h1>
-                        <h4 className="vertical-timeline-element-subtitle" style={{fontSize: "18px"}}>{experience.division}</h4>
+                        <h1 className="vertical-timeline-element-title" style={{ fontSize: "20px" }}>{experience.place}</h1>
+                        <h4 className="vertical-timeline-element-subtitle" style={{ fontSize: "18px" }}>{experience.division}</h4>
                         <p className="vertical-timeline-element-subtitle">{experience.job}</p>
                     </VerticalTimelineElement>
                 )}
