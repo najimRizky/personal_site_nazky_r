93e6ef405d47ba141db20994ba1964ec528060ff
Najim Rizky <najim.rizky@student.umn.ac.id>
2022-02-01 08:58:39 +0700

    add animation on category portfolio

--- src/section/Portfolio.js
+++ src/section/Portfolio.js
@@ -10,9 +10,11 @@ import { Wrap, WrapItem } from '@chakra-ui/react'
 
 import { tags } from "../extras/portfolioData";
 import PortfolioCard from "../components/PortfolioCard";
-import {AnimateSharedLayout, motion } from "framer-motion";
+import {AnimateSharedLayout, motion, useAnimation } from "framer-motion";
 
 const MotionSimpleGrid = motion(SimpleGrid);
+const MotionWrap = motion(Wrap);
+const MotionWrapItem = motion(WrapItem);
 
 
 const Portfolio = (props) => {
@@ -21,12 +23,13 @@ const Portfolio = (props) => {
     const [detail, showDetail] = useState(0);
     const [filter, setFilter] = useState("all");
     const [dataPortfolio, setDataPortfolio] = useState(allPortfolio);
-
+    const animation = useAnimation();
 
 
     useEffect(() => {
         if (inView) {
             window.history.replaceState(null, "", window.location.origin + "/#" + id);
+            animation.start("visible");
         }
         if (!dataPortfolio) {
             setActive()
@@ -52,23 +55,50 @@ const Portfolio = (props) => {
         setDataPortfolio(tmp);
     }
 
+    const BaseVariants = {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const ChildVariants = {
+        hidden: {
+            y: 20,
+            opacity: 0
+        },
+        visible: {
+            y: 0,
+            opacity: 1
+            // transition: {
+            //     when: "beforeChildren",
+            // }
+        }
+    }
+
     return (
         <Box id={id} transition={props.transition} minHeight="100%" pb={10} px={[5, 5, 10, 20]} pt={70} fontFamily="Raleway">
             <Heading color={props.color} ref={ref} mb={10} fontWeight={400} fontSize={50} >
                 PORTFOLIO
             </Heading>
-            <Wrap mb={"20px"}>
+            <MotionWrap variants={BaseVariants} mb={"20px"} initial="hidden" animate={animation}>
                 {tags.map((tag, id) =>
-                    <WrapItem key={id}>
+                    <MotionWrapItem variants={ChildVariants} key={id}>
                         <Tag onClick={() => setNull(tag.id)} className={filter === tag.id ? "filterActive" : ""} px={"25px"} py={"12px"} cursor={"pointer"} borderRadius='full' size="md" _hover={{ bg: "rgba(0,0,0,0.2)" }} transition={"0.2s"} variant='outline' color={props.color}>
                             <TagLabel >{tag.name}</TagLabel>
                             {tag.id !== "all" &&
-                                <Image ml={2} width={"20px"} height={"auto"} src={tag.icon}></Image>
+                                <Image ml={2} width={"20px"} height={"20px"} src={tag.icon}></Image>
                             }
                         </Tag>
-                    </WrapItem>
+                    </MotionWrapItem>
                 )}
-            </Wrap>
+            </MotionWrap>
             <AnimateSharedLayout >
                 <MotionSimpleGrid layout columns={[1, 1, 2, 2]} >
                     {dataPortfolio !== null && dataPortfolio.map((item, id) => (
